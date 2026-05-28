ecf784f89e301b8baf8a7db46fef5e67aa0903b3
Shubham Sharma <shubhamsharma312om@gmail.com>
2026-05-28 12:45:35 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -153,19 +153,16 @@ const transporter = nodemailer.createTransport({
 
   host: "smtp.gmail.com",
 
-  port: 587,
+  port: 465,
 
-  secure: false,
+  secure: true,
 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   },
 
-  tls: {
-  rejectUnauthorized: false
-},
-
+ 
 connectionTimeout: 10000,
 greetingTimeout: 10000,
 socketTimeout: 10000
